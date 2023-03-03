3f7ff4bda13dc00aef474446c103d6a598c72bec
ravitejaY <yrt2492@gmail.com>
2023-03-03 16:45:33 +0530

    storing the user data, tracking the users

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,6 +11,9 @@ const $messages = document.querySelector('#messages');
 const messageTemplate = document.querySelector('#message-template').innerHTML;
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML;
 
+// Options
+const { username, room } = Qs.parse(location.search, {ignoreQueryPrefix: true})
+
 
 socket.on('message', (message) => {
     console.log(message);
@@ -68,3 +71,10 @@ $shareLocationButton.addEventListener('click', () => {
         })
     })
 })
+
+socket.emit('join', { username, room }, (error) => {
+    if(error){
+        alert(error);
+        location.href = '/'
+    }
+});
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const socketio = require('socket.io');
 const Filter = require('bad-words');
 const { generateMessage, generateLocationMessage } = require('./utils/messages');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
 
 const app = express();
 const server = http.createServer(app);
@@ -18,8 +19,20 @@ app.use(express.static(publicDirectoryPath));
 io.on('connection', (socket) => {
     console.log('New Websocket connection')
 
-    socket.emit('message', generateMessage('Welcome!'));
-    socket.broadcast.emit('message', generateMessage('A new user joined!'))
+    socket.on('join', (options, callback) => {
+        const { error, user } = addUser({ id: socket.id, ...options })
+
+        if(error){
+            return callback(error)
+        } 
+    
+        socket.join(user.room)
+
+        socket.emit('message', generateMessage('Welcome!'));
+        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`))
+
+        callback()
+    })
 
     socket.on('sendMessage', (message, callback) => {
         const filter = new Filter();
@@ -28,7 +41,7 @@ io.on('connection', (socket) => {
             return callback('Profanity is not allowed!');
         }
 
-        io.emit('message', generateMessage(message));
+        io.to('Philly').emit('message', generateMessage(message));
         callback();
     })
     socket.on('sendLocation', (coords, callback) => {
@@ -37,7 +50,11 @@ io.on('connection', (socket) => {
     })
 
     socket.on('disconnect', () => {
-        io.emit('message', generateMessage('A user has left'));
+        const user = removeUser(socket.id);
+
+        if(user){
+            io.to(user.room).emit('message', generateMessage(`${user.username} has left!`));
+        }
     })
 })
 
